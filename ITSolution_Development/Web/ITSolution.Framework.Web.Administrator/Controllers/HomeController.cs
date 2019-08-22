9c4d2d8e4db5ed8a771a451985d858dcb6aeccad
Gercy Campos <gercy@outlook.com.br>
2019-08-22 16:26:18 -0300

    Alterações nos arquivos

--- ITSolution_Development/Web/ITSolution.Framework.Web.Administrator/Controllers/HomeController.cs
+++ ITSolution_Development/Web/ITSolution.Framework.Web.Administrator/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace ITSolution.Framework.Web.Administrator.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Software Factory";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact-me";
 
             return View();
         }

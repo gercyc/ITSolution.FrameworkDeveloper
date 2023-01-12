153446995c7217ffcdcbe396116d8f053a5866bb
Gercy Campos <gercy@outlook.com.br>
2023-01-12 13:51:55 -0300

    Update devexpress

--- ITSolution_Development/ITSolution.Admin/Launcher/AdminMenuUtil.cs
+++ ITSolution_Development/ITSolution.Admin/Launcher/AdminMenuUtil.cs
@@ -128,7 +128,7 @@ namespace ITSolution.Admin.Launcher
             //aqui eu ja tenho contexto iniciado
 
             //traz um usuario indexado do banco + login no servidor
-            AdminDTO admin = new AdminDTO(logon, senha, _application.ITSTools.Login(logon, senha));
+            AdminDTO admin = new AdminDTO(logon, senha, true);
 
             
 
